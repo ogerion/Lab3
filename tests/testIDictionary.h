615e9fa706efddbadc2c4bc256320c973f31f2de
Владимир Бредихин <fragebum@gmail.com>
2024-12-22 01:27:56 +0300

    Tests

---
+++ tests/testIDictionary.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "./IDictionary.h"
+#include "cassert"
+void testImydictionary()
+{
+	IDictionary<int, int> mydict;
+	mydict.Add(1, 2);
+	mydict.Add(2, 3);
+	mydict.Add(4, 5);
+	mydict.Add(5, 6);
+
+	int r = mydict.Get(4);
+
+	assert(r == 5);
+	assert(mydict.ContainsKey(1));
+	mydict.erase(2);
+	assert(mydict.GetCount() == 3);
+}
